1f15f495cf05e20bdcff0c8824ba606caa1ff066
Aditya <adityarajak9@gmail.com>
2023-08-29 18:28:36 +0530

    Update url for sales_list

--- billing_app/urls.py
+++ billing_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('purchase/', views.purchase_list, name='purchase_list'),
     path('finance/', views.invoice_list, name='invoice_list'),
     path('hr/', views.department_list, name='department_list'),
+    path('sales/', views.sales_list, name='sales_list'),
     
 ]
